ba2eb0fcdc0af84d8369d856bb9ba494e417f2df
JulieNguyenD <julie_nguyen@outlook.fr>
2016-04-01 17:59:53 +0200

    Javadoc - Application

--- src/Application.java
+++ src/Application.java
@@ -6,26 +6,51 @@ import javax.swing.JFrame;
 import fr.lri.swingstates.canvas.Canvas;
 
 /**
- * Fenêtre de l'application.
- * C'est une JFrame qui contient le canvas
+ * <b>Fenêtre de l'application.</b>
+ * <p>C'est une JFrame qui affiche en plein écran le canvas avec ses composants.
+ * Elle a :
+ * <ul>
+ * <li>Un unique Canvas dans lequel on dessine.</li>
+ * <li>Un WidgetPinceau qui sont les outils de dessin.</li>
+ * <li>Un WidgetCouleur qui est l'outils pour choisir la couleur et la taille du pinceau</li>
+ * <li>Deux Points pour donner la position des deux widgets</li>
+ * </ul>
+ * <p>
+ * 
+ * @see Canvas
+ * @see WidgetPinceau
+ * @see Point
  * 
  * @author ANDRIANIRINA Tojo
  * @author GABRIEL Damien
  * @author NGUYEN Julie
  */
-@SuppressWarnings("serial")
 public class Application extends JFrame {
-	
+	/**
+	 * Le Canvas de la fenêtre. On dessine dessus et les CElements sont placés dessus.
+	 * @see Application#Application()
+	 */
 	private Canvas canvas;
-	private Point positionWidgetP;
+	
+	/**
+	 * Le widget contenant les outils de dessin : pinceau, gomme, pot et formes.
+	 * @see Application#Application()
+	 */
 	private WidgetPinceau widgetpinceau;
+	
+	/**
+	 * Le point de positionnement du Widget Pinceau.
+	 * @see Application#Application()
+	 */
+	private Point positionWidgetP;
 	
 	public Application () {
 		super("Application Dessin - A.G.N");
 		
 		Dimension minsize = new Dimension(600,600);
-		this.setPreferredSize(minsize);
+		//this.setPreferredSize(minsize);
 		this.setMinimumSize(minsize);
+		this.setExtendedState(this.MAXIMIZED_BOTH);
 		
 		positionWidgetP = new Point(400, 100);
 		

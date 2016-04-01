7bae7b5f64dce7d660f5ef85fd8c25d5915e6d31
JulieNguyenD <julie_nguyen@outlook.fr>
2016-04-01 17:18:23 +0200

    Javadoc - Classe Lancement

--- src/Lancement.java
+++ src/Lancement.java
@@ -5,8 +5,10 @@ import fr.lri.swingstates.debug.StateMachineVisualization;
 import fr.lri.swingstates.sm.JStateMachine;
 
 /**
- * Lancement de l'application.
- * On lance new Application()
+ * <b>Lancement de l'application.</b>
+ * <p>Fonction main qui fait une nouvelle fenêtre Application</p>
+ *  
+ * @see Application
  *  
  * @author ANDRIANIRINA Tojo
  * @author GABRIEL Damien
@@ -18,13 +20,29 @@ public class Lancement {
 		new Application();
 	}
 	
+	/**
+	 * <b>Affiche la CStateMachine sm.</b>
+	 * <p>Ouvre une nouvelle fenêtre avec l'état de la CStateMachine sm.</p>
+	 * 
+	 * @see CStateMachine
+	 * 
+	 * @param sm
+	 */
 	public static void showStateMachine(CStateMachine sm) {
         JFrame viz = new JFrame();
         viz.getContentPane().add(new StateMachineVisualization(sm));
         viz.pack();
         viz.setVisible(true);
     }
-
+	
+	/**
+	 * <b>Affiche la JStateMachine sm</b>
+	 * <p>Ouvre une nouvelle fenêtre avec l'état de la JStateMachine sm.</p>
+	 * 
+	 * @see JStateMachine
+	 * 
+	 * @param sm
+	 */
     public static void showJStateMachine(JStateMachine sm) {
         JFrame viz = new JFrame();
         viz.getContentPane().add(new StateMachineVisualization(sm));

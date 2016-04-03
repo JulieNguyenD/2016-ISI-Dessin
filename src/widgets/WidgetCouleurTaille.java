0bc59690442b414c427b458ef0057bfe9c3fbb55
tojoiv <tojoiv@yahoo.fr>
2016-04-03 20:44:18 +0200

    ajout fichier WidgetCouleurTaille

---
+++ src/widgets/WidgetCouleurTaille.java
@@ -0,0 +1,10 @@
+package widgets;
+
+import fr.lri.swingstates.canvas.CRectangle;
+import fr.lri.swingstates.canvas.Canvas;
+
+public class WidgetCouleurTaille {
+	private Canvas canvas;
+	private CRectangle drag;
+	
+}

0f69f42071940245d89edb22bbc68687fba8efe6
JulieNguyenD <julie_nguyen@outlook.fr>
2016-04-07 21:46:55 +0200

    Javadoc Taille terminée

--- src/elements/Taille.java
+++ src/elements/Taille.java
@@ -3,24 +3,38 @@ package elements;
 import java.awt.BasicStroke;
 import java.awt.Color;
 
-import javax.swing.JFrame;
-
 import fr.lri.swingstates.canvas.CRectangle;
 import fr.lri.swingstates.canvas.CStateMachine;
 import fr.lri.swingstates.canvas.Canvas;
-import fr.lri.swingstates.canvas.transitions.EnterOnShape;
-import fr.lri.swingstates.canvas.transitions.LeaveOnShape;
-import fr.lri.swingstates.debug.StateMachineVisualization;
-import fr.lri.swingstates.sm.State;
-import fr.lri.swingstates.sm.Transition;
-import fr.lri.swingstates.sm.transitions.Press;
-import fr.lri.swingstates.sm.transitions.Release;
 
+/**
+ * <b>CRectangle pour la Taille </b>
+ * <p>C'est un carré qui possède un CRectangle avec une certaine taille.</p>
+ * 
+ * @see CRectangle
+ * 
+ * @author ANDRIANIRINA Tojo
+ * @author GABRIEL Damien
+ * @author NGUYEN Julie
+ */
 public class Taille extends CRectangle {
+	
+	/**
+	 * Le CRectangle avec la taille différente.
+	 */
 	private CRectangle rectangle;
-	private CStateMachine sm;
+	
+	/**
+	 * La taille du CRectangle rectangle.
+	 */
 	private double tailleThis;
 
+	/**
+	 * Constructeur de Taille.
+	 * <p>A la création de Taille, on initialise le rectangle avec la taille.</p>
+	 * @param taille : la taille du rectangle.
+	 * @param canvas : le canvas sur lequel on le place.
+	 */
 	public Taille (final double taille, Canvas canvas){
 		super (0, 0, 50, 50);
 		rectangle = canvas.newRectangle(5, 25, 40, taille);
@@ -39,64 +53,21 @@ public class Taille extends CRectangle {
 		this.below(rectangle);			
 		
 		this.addTag("taille");
-		
-//		sm = new CStateMachine (){
-//			public State out = new State() {
-//				public void enter() {					
-//					setFillPaint(Color.white);
-//				}
-//				
-//				Transition toOver = new EnterOnShape(">> over") {};
-//				Transition pressOut = new Press (">> disarmed") {};
-//			};
-//
-//			public State over = new State() {
-//				public void enter() {					
-//					setFillPaint(Color.gray);
-//				}				
-//				
-//				Transition leave = new LeaveOnShape(">> out") {};
-//				Transition arm = new Press(BUTTON1,">> armed") {};
-//
-//			};
-//
-//			public State armed = new State() {
-//				public void enter() {
-//					setFillPaint(Color.blue);
-//				}
-//
-//				Transition disarm = new LeaveOnShape(">> disarmed") {
-//					public void action (){
-//						tailleThis = taille;
-//						System.out.println("test reussi =========================");
-//					}
-//				};
-//				Transition act = new Release(BUTTON1, ">> over") {};
-//				
-//			};
-//
-//			public State disarmed = new State() {
-//				public void enter() {
-//					setFillPaint(Color.white);
-//					// Faire ici les grandes actions
-//					// il y a un choix de couleur quelque part qui se fait
-//					
-//				}
-//
-//				Transition rearm = new EnterOnShape(">> armed") {};
-//				Transition cancel = new Release(BUTTON1, ">> out") {};
-//
-//			};
-//		};
-//		
-//		sm.attachTo(this);
 	}
 	
+	/**
+	 * Getter de Taille
+	 * @return tailleThis : la taille du rectangle.
+	 */
 	public double getTaille() {
 		return this.tailleThis;
 		
 	}
 	
+	/**
+	 * Fonction qui permet de montrer ou cacher la taille. On ne peut plus récupérer les états avec un CStateMachine.
+	 * @param b : true si on veut montrer. False si on veut cacher.
+	 */
 	public void montrer(boolean b) {
 		this.setDrawable(b);
 		this.setPickable(b);

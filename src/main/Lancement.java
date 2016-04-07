96bfcf46c6cc901b9e26b66b31608c51873a41e9
JulieNguyenD <julie_nguyen@outlook.fr>
2016-04-07 22:01:04 +0200

    Javadoc Lancement terminée

--- src/main/Lancement.java
+++ src/main/Lancement.java
@@ -2,12 +2,6 @@ package main;
 
 import java.io.IOException;
 
-import javax.swing.JFrame;
-
-import fr.lri.swingstates.canvas.CStateMachine;
-import fr.lri.swingstates.debug.StateMachineVisualization;
-import fr.lri.swingstates.sm.JStateMachine;
-
 /**
  * <b>Lancement de l'application.</b>
  * <p>Fonction main qui fait une nouvelle fenêtre Application</p>

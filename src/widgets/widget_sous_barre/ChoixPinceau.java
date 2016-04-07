f610238028e65c22abc318aa893917be1830c67e
JulieNguyenD <julie_nguyen@outlook.fr>
2016-04-07 22:19:26 +0200

    Javadoc ChoixPinceau terminée

--- src/widgets/widget_sous_barre/ChoixPinceau.java
+++ src/widgets/widget_sous_barre/ChoixPinceau.java
@@ -3,30 +3,48 @@ package widgets.widget_sous_barre;
 import java.awt.BasicStroke;
 import java.awt.geom.Point2D;
 
-import elements.Couleur;
-import elements.Pinceau;
-import elements.Taille;
 import fr.lri.swingstates.canvas.CRectangle;
-import fr.lri.swingstates.canvas.CShape;
-import fr.lri.swingstates.canvas.CStateMachine;
 import fr.lri.swingstates.canvas.Canvas;
-import fr.lri.swingstates.canvas.transitions.EnterOnTag;
-import fr.lri.swingstates.canvas.transitions.LeaveOnTag;
-import fr.lri.swingstates.canvas.transitions.PressOnShape;
-import fr.lri.swingstates.canvas.transitions.PressOnTag;
-import fr.lri.swingstates.sm.State;
-import fr.lri.swingstates.sm.Transition;
-import fr.lri.swingstates.sm.transitions.Press;
-import fr.lri.swingstates.sm.transitions.Release;
-import main.Utilitaires;
 
+/**
+ * <b>CRectangle pour le Choix du Pinceau </b>
+ * <p>C'est un CRectangle qui possède les choix pour le pinceau.</p>
+ * 
+ * @see CRectangle
+ * 
+ * @author ANDRIANIRINA Tojo
+ * @author GABRIEL Damien
+ * @author NGUYEN Julie
+ */
 public class ChoixPinceau extends CRectangle {
 	
+	/**
+	 * Canvas sur lequel on dessine.
+	 */
 	private Canvas canvas;
+	
+	/**
+	 * Les choix pour la couleur du pinceau
+	 * @see Couleur_Widget
+	 */
 	private Couleur_Widget choixPinceauCouleur;
+	
+	/**
+	 * Les choix pour la taille du pinceau
+	 * @see Taille_Widget
+	 */
 	private Taille_Widget choixPinceauTaille;
+	
+	/**
+	 * Le point de départ du widget annexe.
+	 */
 	private Point2D position_widget;
 	
+	/**
+	 * Constructeur de ChoixPinceau. Au départ, il n'est pas visible.
+	 * @param canvas : canvas sur lequel on dessine, ajoute les éléments.
+	 * @param position : position de départ du widget annexe.
+	 */
 	public ChoixPinceau(Canvas canvas, Point2D position) {
 		super(position.getX()-70*2-10*2, position.getY(), 70*2, 290);
 		this.canvas = canvas;
@@ -50,6 +68,10 @@ public class ChoixPinceau extends CRectangle {
 		this.montrer(false);			
 	}
 	
+	/**
+	 * Fonction qui permet de montrer ou cacher le widget. On ne peut plus récupérer les états avec un CStateMachine.
+	 * @param b : true si on veut montrer. False si on veut cacher.
+	 */
 	public void montrer(boolean b) {
 		this.setDrawable(b);
 		this.setPickable(b);

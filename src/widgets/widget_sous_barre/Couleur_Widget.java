ade443b2ae6bb9c81b8c277b2f7c293ec57a4390
JulieNguyenD <julie_nguyen@outlook.fr>
2016-04-07 22:29:59 +0200

    Javadoc CouleurWidget terminée

--- src/widgets/widget_sous_barre/Couleur_Widget.java
+++ src/widgets/widget_sous_barre/Couleur_Widget.java
@@ -4,21 +4,48 @@ import java.awt.Color;
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
 
-import javax.swing.JFrame;
-
 import elements.Couleur;
 import fr.lri.swingstates.canvas.CRectangle;
-import fr.lri.swingstates.canvas.CShape;
-import fr.lri.swingstates.canvas.CStateMachine;
 import fr.lri.swingstates.canvas.Canvas;
 
+/**
+ * <b>CRectangle pour mettre les différents choix de couleurs </b>
+ * <p>C'est un CRectangle qui possède les choix pour la Couleur.</p>
+ * 
+ * @see CRectangle
+ * 
+ * @author ANDRIANIRINA Tojo
+ * @author GABRIEL Damien
+ * @author NGUYEN Julie
+ */
 public class Couleur_Widget extends CRectangle {
 	
+	/**
+	 * Canvas sur lequel on dessine.
+	 */
 	private Canvas canvas;
+	
+	/**
+	 * Liste des Couleurs
+	 * @see Couleur
+	 */
 	private ArrayList <Couleur> couleurList;
+	
+	/**
+	 * Le padding.
+	 */
 	private int padding = 10;
+	
+	/**
+	 * La position de depart.
+	 */
 	private Point2D position_init;
 
+	/**
+	 * Constructeur de Couleur_Widget. Au départ, il n'est pas visible.
+	 * @param canvas : canvas sur lequel on dessine, ajoute les éléments.
+	 * @param position : position de départ du widget annexe.
+	 */
 	public Couleur_Widget (Canvas canvas, Point2D position){
 		super(position, 70, 290);
 		this.canvas = canvas;	
@@ -39,6 +66,10 @@ public class Couleur_Widget extends CRectangle {
 				
 	}
 	
+	/**
+	 * Fonction qui permet de montrer ou cacher le widget. On ne peut plus récupérer les états avec un CStateMachine.
+	 * @param b : true si on veut montrer. False si on veut cacher.
+	 */
 	public void montrer(boolean b) {
 		for(Couleur i : couleurList) {
 			i.montrer(b);
